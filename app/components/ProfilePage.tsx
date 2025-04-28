f25eaf879b3e0a1cf6b6ad5c161ec15a74b9dc46
Nikita Likinykh <likinykh.work@gmail.com>
2025-04-28 18:02:05 +0200

    add tabs

--- app/components/ProfilePage.tsx
+++ app/components/ProfilePage.tsx
@@ -1,5 +1,6 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
 import InputText from "./Form/InputText";
 import InputSelect from "./Form/InputSelect";
 import Switch from "./Form/Switch";
@@ -12,9 +13,22 @@ interface ProfilePageProps {
   };
 }
 export default function ProfilePage({ user }: ProfilePageProps) {
+  const router = useRouter();
+  const [activeTab, setActiveTab] = useState("settings");
   const [email, setEmail] = useState(user.email || "");
   const [oldPassword, setOldPassword] = useState("");
   const [newPassword, setNewPassword] = useState("");
+
+  useEffect(() => {
+    const queryTab = new URLSearchParams(window.location.search).get("tab");
+    if (queryTab) setActiveTab(queryTab);
+  }, []);
+
+  const handleTabChange = (tab: string) => {
+    setActiveTab(tab);
+    router.push(`?tab=${tab}`);
+  };
+
   return (
     <div className="">
       <div className="bg-[#0069FF] py-6">
@@ -22,56 +36,91 @@ export default function ProfilePage({ user }: ProfilePageProps) {
           Кабинет пользователя
         </div>
         <div className="flex justify-center p-0 mx-auto my-8 bg-[#2B82FF] rounded-lg  w-fit max-sm:flex-wrap max-sm:h-auto max-sm:w-[90%]">
-          <div className="px-6 pt-1 pb-1 text-lg font-semibold  cursor-pointer max-sm:flex-auto max-sm:px-4 max-sm:py-2 bg-white text-blue-500 rounded-md">
+          <div
+            className={`px-6 pt-1 pb-1 text-lg font-semibold cursor-pointer max-sm:flex-auto max-sm:px-4 max-sm:py-2 ${
+              activeTab === "settings"
+                ? "bg-white text-blue-500 rounded-md"
+                : "text-white"
+            }`}
+            onClick={() => handleTabChange("settings")}
+          >
             Настройки
           </div>
-          <div className="px-6 pt-1 pb-1 text-lg font-semibold text-white cursor-pointer max-sm:flex-auto max-sm:px-4 max-sm:py-2">
+          <div
+            className={`px-6 pt-1 pb-1 text-lg font-semibold cursor-pointer max-sm:flex-auto max-sm:px-4 max-sm:py-2 ${
+              activeTab === "notifications"
+                ? "bg-white text-blue-500 rounded-md"
+                : "text-white"
+            }`}
+            onClick={() => handleTabChange("notifications")}
+          >
             Уведомления
           </div>
-          <div className="px-6 pt-1 pb-1 text-lg font-semibold text-white cursor-pointer max-sm:flex-auto max-sm:px-4 max-sm:py-2">
+          <div
+            className={`px-6 pt-1 pb-1 text-lg font-semibold cursor-pointer max-sm:flex-auto max-sm:px-4 max-sm:py-2 ${
+              activeTab === "referral"
+                ? "bg-white text-blue-500 rounded-md"
+                : "text-white"
+            }`}
+            onClick={() => handleTabChange("referral")}
+          >
             Реферальная программа
           </div>
         </div>
       </div>
-      <div className="bg-white p-4 py-10">
-        <div className="max-w-[400px] w-full mx-auto flex flex-col gap-8">
-          <p className="text-2xl font-bold">Личные Данные</p>
-          <InputText
-            label="E-mail"
-            value={email}
-            onChange={(ev) => setEmail(ev.target.value)}
-          />
-          <InputSelect />
-        </div>
-      </div>
-      <div className="bg-white p-4 ">
-        <div className="max-w-[400px] w-full mx-auto flex flex-col gap-8">
-          <p className="text-2xl font-bold">Уведомление</p>
-          <div className="flex justify-between">
-            <p className="text-lg font-light">Получать уведомления на Email</p>
-            <Switch />
+      {activeTab === "settings" && (
+        <div className="bg-white p-4 py-10">
+          <div className="max-w-[400px] w-full mx-auto flex flex-col gap-8">
+            <p className="text-2xl font-bold">Личные Данные</p>
+            <InputText
+              label="E-mail"
+              value={email}
+              onChange={(ev) => setEmail(ev.target.value)}
+            />
+            <InputSelect />
+            <p className="text-2xl font-bold">Уведомление</p>
+            <div className="flex justify-between">
+              <p className="text-lg font-light">
+                Получать уведомления на Email
+              </p>
+              <Switch />
+            </div>
+
+            <p className="text-2xl font-bold">Изменить пароль</p>
+            <InputPassword
+              label="Старый пароль"
+              value={oldPassword}
+              onChange={(ev) => {
+                setOldPassword(ev.target.value);
+              }}
+            />
+            <InputPassword
+              label="Новый пароль"
+              value={newPassword}
+              onChange={(ev) => {
+                setNewPassword(ev.target.value);
+              }}
+            />
           </div>
         </div>
-      </div>
-      <div className="bg-white p-4 ">
-        <div className="max-w-[400px] w-full mx-auto flex flex-col gap-8">
-          <p className="text-2xl font-bold">Изменить пароль</p>
-          <InputPassword
-            label="Старый пароль"
-            value={oldPassword}
-            onChange={(ev) => {
-              setOldPassword(ev.target.value);
-            }}
-          />
-          <InputPassword
-            label="Новый пароль"
-            value={newPassword}
-            onChange={(ev) => {
-              setNewPassword(ev.target.value);
-            }}
-          />
+      )}
+      {activeTab === "notifications" && (
+        <div className="bg-white p-4 ">
+          <div className="max-w-[400px] w-full mx-auto flex flex-col gap-8 my-18">
+            <p className="text-3xl font-normal text-center">Нет уведомлений</p>
+          </div>
         </div>
-      </div>
+      )}
+      {activeTab === "referral" && (
+        <div className="bg-white p-4 ">
+          <div className="max-w-[400px] w-full mx-auto flex flex-col gap-8">
+            <p className="text-3xl font-normal text-center">
+              {" "}
+              Операции не найдены
+            </p>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
